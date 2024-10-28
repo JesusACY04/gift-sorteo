f09681c820ec843294a9c96c834e5b137b978412
Jesús Antonio Ceme Yam <131736251+JesusACY04@users.noreply.github.com>
2024-10-28 00:00:24 -0500

    Update participantes.js

--- backend/models/participantes.js
+++ backend/models/participantes.js
@@ -2,10 +2,10 @@
 const mongoose = require("mongoose");
 
 const participanteSchema = new mongoose.Schema({
-    telefono: { type: String, required: true },
+    telefono: { type: String, required: true, match: /^[0-9]{10}$/ },
     nombre: { type: String, required: true },
-    correo: { type: String, required: true },
-    numeroControl: { type: String, required: true }
+    correo: { type: String, required: true, match: /^L\d{8}@cancun\.tecnm\.mx$/ },
+    numeroControl: { type: String, required: true, match: /^\d{8}$/ }
 });
 
 module.exports = mongoose.model("Participante", participanteSchema);
