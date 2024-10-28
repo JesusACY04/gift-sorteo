2a46c8316e35efc0f240da1fdb44a8255adfaaaa
Jesús Antonio Ceme Yam <131736251+JesusACY04@users.noreply.github.com>
2024-10-27 22:38:09 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,16 @@ app.use(express.json());
 
 // Conexión a MongoDB usando la URI de conexión en el archivo .env
 mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log("Conectado a MongoDB"))
-    .catch(error => console.error("Error de conexión:", error));
+    .then(() => {
+        console.log("Conectado a MongoDB");
+        app.listen(PORT, () => {
+            console.log(`Servidor en funcionamiento en el puerto ${PORT}`);
+        });
+    })
+    .catch(error => {
+        console.error("Error de conexión:", error);
+        process.exit(1); // Termina el proceso si falla la conexión
+    });
 
 // Importar y usar el enrutador de participantes
 app.use("/api/participantes", require("./routes/participantes"));
